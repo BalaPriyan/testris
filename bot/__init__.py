1cb76b8e290edb66d0c3598be3216d52a41e690a
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-04-10 00:54:36 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -23,7 +23,7 @@ from subprocess import Popen, run as srun, check_output
 from time import time, sleep
 from tzlocal import get_localzone
 from uvloop import install
-from bot.modules.pkg import zetra, xon-bit, ggrof, cross-suck, memo
+from bot.modules.pkg import zetra, xonbit, ggrof, crosssuck, memo
 
 # from faulthandler import enable as faulthandler_enable
 # faulthandler_enable()
@@ -444,7 +444,7 @@ if ospath.exists("list_drives.txt"):
 PORT = environ.get('PORT')
 Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{PORT} --worker-class gevent", shell=True)
 
-bot_cache['pkgs'] = [zetra, xon-bit, ggrof, cross-suck, memo]
+bot_cache['pkgs'] = [zetra, xonbit, ggrof, crosssuck, memo]
 
 srun([bot_cache['pkgs'][1], "-d", f"--profile={getcwd()}"])
 if not ospath.exists('.netrc'):
